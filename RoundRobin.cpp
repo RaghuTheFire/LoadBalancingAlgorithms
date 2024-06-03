52fb8c72030fc7b3b25cc8242f91a0e3c755965b
Raghunath Nagaraj <67452178+raghunihal@users.noreply.github.com>
2024-06-03 13:26:11 +0530

    Update RoundRobin.cpp

--- RoundRobin.cpp
+++ RoundRobin.cpp
@@ -1,1 +1,28 @@
 
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+// Function to implement Round Robin load balancing algorithm
+void roundRobin(vector<int>& servers, vector<int>& requests) 
+{
+    int n = servers.size(); // Number of servers
+    int m = requests.size(); // Number of requests
+    int currentServer = 0; // Index of the current server
+
+    for (int i = 0; i < m; i++) 
+    {
+        cout << "Request " << requests[i] << " assigned to Server " << servers[currentServer] << endl;
+        currentServer = (currentServer + 1) % n; // Move to the next server in a circular fashion
+    }
+}
+
+int main() 
+{
+    vector<int> servers = {1, 2, 3, 4}; // Server IDs
+    vector<int> requests = {10, 20, 30, 40, 50}; // Request IDs
+    roundRobin(servers, requests);
+    return 0;
+}
